0655dc157d359004d91a59e4a8479e68c7dd08dd
jnralexandre <alexandreoliveirajnr@gmail.com>
2023-12-05 19:32:37 -0300

    commit 2

---
+++ src/main/java/br/org/fundatec/aula03/Model/Estado.java
@@ -0,0 +1,7 @@
+package br.org.fundatec.aula03.Model;
+
+public enum Estado {
+    MG,
+    RS
+
+}
---
+++ src/main/java/br/org/fundatec/aula03/Model/Loja.java
@@ -0,0 +1,56 @@
+package br.org.fundatec.aula03.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Loja {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private long id;
+
+    @Column(length = 50, unique = true, nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private  Integer numeroCarros;
+
+
+    @OneToOne
+    private Endereco endereco;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumeroCarros() {
+        return numeroCarros;
+    }
+
+    public void setNumeroCarros(Integer numeroCarros) {
+        this.numeroCarros = numeroCarros;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+
+}
